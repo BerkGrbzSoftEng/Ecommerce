ebd4b66bd5ff917810b9a1fa7fec4f0c085536cd
berkgrbzsofteng <berk@bswcube.net>
2020-12-27 17:33:35 +0300

    Changes

---
+++ Core/CrossCuting/Validation/ValidatorTool.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation;
+
+namespace Core.CrossCuting.Validation
+{
+    public class ValidatorTool
+    {
+        public static void FluentValidation(IValidator validator,object entity)
+        {
+            var result = validator.ValidateAsync((IValidationContext)entity);
+            if (result.Result.Errors.Count>0)
+            {
+                throw new ValidationException(result.Result.Errors);
+            }
+
+
+        }
+    }
+}
